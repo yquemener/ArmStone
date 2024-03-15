1996dbda6d9e2a7af5cfcf59763f04a7ac47ed8e
Yves Quemener <yves.quemener@gmail.com>
2024-03-15 19:49:46 +0100

    New UI

--- orchestrator/UI.py
+++ orchestrator/UI.py
@@ -1,16 +1,17 @@
 import sys
-import time
-from xarm.wrapper import XArmAPI
-import PyQt5
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QGridLayout
-from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtGui import QImage, QPixmap
 import cv2
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QRectF
+from PyQt5.QtGui import QImage, QPixmap, QPainter
+import sys
+import cv2
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QLabel, QHBoxLayout
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QRect
+from PyQt5.QtGui import QImage, QPixmap
 import os
 from pathlib import Path
-from PyQt5.QtCore import QTimer, QSize
-from PyQt5.QtGui import QImage, QPixmap
-
+import PyQt5
+from xarm.wrapper import XArmAPI
 
 # Hack to make PyQt and cv2 load simultaneously
 os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = os.fspath(
@@ -18,69 +19,126 @@ os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = os.fspath(
 )
 
 
-class VideoPlayer(QWidget):
+class VideoThread(QThread):
+    change_pixmap_signal = pyqtSignal(QImage)
+
+    def __init__(self, video_path):
+        super().__init__()
+        self.video_path = video_path
+
+    def run(self):
+        cap = cv2.VideoCapture(self.video_path)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgb_image.shape
+                bytes_per_line = ch * w
+                qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+                self.change_pixmap_signal.emit(qt_image)
+            else:
+                break
+        cap.release()
+
+
+class VideoView(QWidget):
+    def __init__(self, video_path):
+        super().__init__()
+        self.setWindowTitle("Video View")
+
+        self.scene = QGraphicsScene()
+        self.view = QGraphicsView(self.scene)
+        self.view.setRenderHint(QPainter.Antialiasing)
+
+        self.zoom_button = QPushButton("Zoom", self)
+        self.zoom_button.setCheckable(True)
+        self.zoom_button.toggled.connect(self.toggle_zoom)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.view)
+        self.layout.addWidget(self.zoom_button)
+        self.setLayout(self.layout)
+
+        self.video_item = QGraphicsPixmapItem()
+        self.scene.addItem(self.video_item)
+
+        self.thread = VideoThread(video_path)
+        self.thread.change_pixmap_signal.connect(self.update_image)
+        self.thread.start()
+
+        self.is_zoomed = False
+
+    def update_image(self, qt_img):
+        pixmap = QPixmap.fromImage(qt_img)
+        self.video_item.setPixmap(pixmap)
+        if not self.zoom_button.isChecked():
+            self.view.fitInView(self.video_item, Qt.KeepAspectRatio)
+
+    def toggle_zoom(self, checked):
+        if checked:
+            # When zoom is checked, show the video unscaled (cropped by the QGraphicsView).
+            self.view.setDragMode(QGraphicsView.ScrollHandDrag)
+            self.view.resetTransform()  # Reset any scaling to show the video unscaled.
+        else:
+            # When zoom is unchecked, scale the video to fit the QGraphicsView.
+            self.view.setDragMode(QGraphicsView.NoDrag)
+            self.view.fitInView(self.video_item, Qt.KeepAspectRatio)
+
+
+class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Video Player")
-        self.setGeometry(100, 100, 1200, 800)  # Start window bigger
         self.initUI()
-        self.initCameras()
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.updateFrame)
-        self.timer.start(30)  # Update interval in ms
-        # self.arm = XArmAPI("192.168.1.212")
-        # self.arm.motion_enable(enable=True)
-        # self.arm.set_mode(5)
-        # self.arm.set_state(state=0)
-        # self.arm.reset(wait=True)
+        try:
+            self.arm = XArmAPI("192.168.1.212")
+            self.arm.motion_enable(enable=True)
+            self.arm.set_mode(5)
+            self.arm.set_state(state=0)
+            self.arm.reset(wait=True)
+        except Exception:
+            print ("Could not initialize the arm")
 
     def initUI(self):
-        # Root vertical layout
-        rootVLayout = QVBoxLayout()
+        self.layout = QVBoxLayout()
 
-        # Video Labels with larger size
-        self.videoLabel32 = QLabel(self)
-        self.videoLabel33 = QLabel(self)
-        self.videoLabel2 = QLabel(self)
+        self.setLayout(self.layout)
 
-        # Set a larger minimum size for the video display areas
-        # self.videoLabel32.setMinimumSize(QSize(800, 600))
-        # self.videoLabel33.setMinimumSize(QSize(800, 600))
-        # self.videoLabel2.setMinimumSize(QSize(800, 600))
-
-
-        # Horizontal layouts for buttons
-        hLayoutX = QHBoxLayout()
+        videoLayout = QHBoxLayout()
+        v1Layout = QVBoxLayout()
+        self.video_view1 = VideoView('/dev/video32')
+        v1Layout.addWidget(self.video_view1)
+        v1buttons = QHBoxLayout()
         self.btnMinusX = QPushButton("-X")
         self.btnPlusX = QPushButton("+X")
-        hLayoutX.addWidget(self.btnMinusX)
-        hLayoutX.addWidget(self.btnPlusX)
+        v1buttons.addWidget(self.btnPlusX)
+        v1buttons.addWidget(self.btnMinusX)
+        v1Layout.addLayout(v1buttons)
+        videoLayout.addLayout(v1Layout)
 
-        hLayoutY = QHBoxLayout()
+        v2Layout = QVBoxLayout()
+        self.video_view2 = VideoView('/dev/video33')
+        v2Layout.addWidget(self.video_view2)
+        v2buttons = QHBoxLayout()
         self.btnMinusY = QPushButton("-Y")
         self.btnPlusY = QPushButton("+Y")
-        hLayoutY.addWidget(self.btnMinusY)
-        hLayoutY.addWidget(self.btnPlusY)
+        v2buttons.addWidget(self.btnMinusY)
+        v2buttons.addWidget(self.btnPlusY)
+        v2Layout.addLayout(v2buttons)
+        videoLayout.addLayout(v2Layout)
 
-        rootVLayout.addWidget(self.videoLabel33)
-        rootVLayout.addLayout(hLayoutX)
-        rootVLayout.addWidget(self.videoLabel32)
-        rootVLayout.addLayout(hLayoutY)
-        rootVLayout.addWidget(self.videoLabel2)
-
-        # Vertical layout for Z and STOP buttons
-        vLayoutZ = QVBoxLayout()
+        zButLayout = QVBoxLayout()
         self.btnPlusZ = QPushButton("+Z")
         self.btnMinusZ = QPushButton("-Z")
         self.btnStop = QPushButton("STOP")
-        vLayoutZ.addWidget(self.btnPlusZ)
-        vLayoutZ.addWidget(self.btnMinusZ)
-        vLayoutZ.addWidget(self.btnStop)
+        zButLayout.addWidget(self.btnPlusZ)
+        zButLayout.addWidget(self.btnMinusZ)
+        zButLayout.addWidget(self.btnStop)
+        videoLayout.addLayout(zButLayout)
 
-        # Add Z control vertical layout to the root layout
-        rootVLayout.addLayout(vLayoutZ)
+        self.layout.addLayout(videoLayout)
 
-        # Replace or add these in the addControlButtons method or where you initialize buttons
         self.btnMinusX.pressed.connect(lambda: self.buttonPressedAction((-1, 0, 0)))
         self.btnMinusX.released.connect(lambda: self.buttonReleasedAction((-1, 0, 0)))
         self.btnPlusX.pressed.connect(lambda: self.buttonPressedAction((1, 0, 0)))
@@ -96,37 +154,7 @@ class VideoPlayer(QWidget):
         self.btnMinusZ.pressed.connect(lambda: self.buttonPressedAction((0, 0, -1)))
         self.btnMinusZ.released.connect(lambda: self.buttonReleasedAction((0, 0, -1)))
 
-        self.setLayout(rootVLayout)
-
-    def initCameras(self):
-        # Initialize camera captures
-        # self.cap32 = cv2.VideoCapture('/dev/video32')
-        # self.cap33 = cv2.VideoCapture('/dev/video33')
-        # self.cap2 = cv2.VideoCapture('/dev/video2')
-        self.cap32 = cv2.VideoCapture('/dev/video0')
-        self.cap33 = cv2.VideoCapture('/dev/video2')
-        self.cap2 = cv2.VideoCapture('/dev/video2')
-
-    def updateFrame(self):
-        # Read frame from each camera and update on respective QLabel
-        for cap, label in [(self.cap32, self.videoLabel32), (self.cap33, self.videoLabel33), (self.cap2, self.videoLabel2)]:
-            ret, frame = cap.read()
-            if ret:
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                h, w, ch = rgbImage.shape
-                bytesPerLine = ch * w
-                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(label.width(), label.height(), aspectRatioMode=Qt.KeepAspectRatio)
-                label.setPixmap(QPixmap.fromImage(p))
-
-    # def buttonPressed(self, vector):
-    #     # Placeholder function for button press actions
-    #     print(f"Button pressed with vector: {vector}")
-    #     p = self.arm.position
-    #     m = 1
-    #     self.arm.set_position(x=p[0] + vector[0] * m,
-    #                           y=p[1] + vector[1] * m,
-    #                           z=p[2] + vector[2] * m)
+        self.btnStop.clicked.connect(self.stopFunction)
 
     def buttonPressedAction(self, vector):
         v=list(vector)
@@ -136,13 +164,12 @@ class VideoPlayer(QWidget):
         v[2] *= mult
         self.arm.vc_set_cartesian_velocity(speeds=[v[0], v[1], v[2], 0, 0, 0],
                                            duration=1)
-        print(f"Button pressed with vector: {vector}")
-        # Your pressed action logic here
 
     def buttonReleasedAction(self, vector):
         self.arm.vc_set_cartesian_velocity(speeds=[0, 0, 0, 0, 0, 0])
-        print(f"Button released with vector: {vector}")
-        # Your released action logic here
+
+    def stopFunction(self):
+        self.arm.set_state(4)
 
     def closeEvent(self, event):
         # Release resources
@@ -154,12 +181,8 @@ class VideoPlayer(QWidget):
         self.cap33.release()
         self.cap2.release()
 
-    def stopFunction(self):
-        self.arm.set_state(4)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = VideoPlayer()
-    ex.show()
+    main_win = MainWidget()
+    main_win.show()
     sys.exit(app.exec_())
