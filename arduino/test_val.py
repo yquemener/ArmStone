5d52dd5cbc41bf627780e8bcde5a2118d0e10fc9
Yves Quemener <yves.quemener@gmail.com>
2024-07-23 00:50:07 +0200

    Small modifications to the clamp arduino testing code

--- arduino/test_val.py
+++ arduino/test_val.py
@@ -8,7 +8,7 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=10,
 			bytesize=serial.EIGHTBITS)
 time.sleep(0.3)
 
-for k in range(-180,360,5):
+for k in range(0,180,5):
     print(k)
     ser.write("\n\n\n".encode('utf-8'))
     cmd = f"111 {k} 222\n"
